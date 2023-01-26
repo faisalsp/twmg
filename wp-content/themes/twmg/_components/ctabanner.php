9de16c4ce6fa00bcab9ac786ffb4eb51ee761cf5
faisal <faisal@twmg.com.au>
2023-01-26 15:54:51 +0700

    add underscore image on CTA banner

--- wp-content/themes/twmg/_components/ctabanner.php
+++ wp-content/themes/twmg/_components/ctabanner.php
@@ -1,7 +1,10 @@
 <div class="container">
     <div class="outer-border mx-auto">
-        <div class="cta-banner text-center ">
-            <h4>Unlock Your Business Potential with a Tailored Digital Marketing Strategy and deliver skyhigh roi</h4>
+        <div class="cta-banner text-center">
+            <div class="title">
+                <h4>Unlock Your Business Potential with a Tailored Digital Marketing Strategy and deliver <span class="underscore position-relative">skyhigh roi</span>
+                </h4>
+            </div>
             <p>Our team of experts will work with you to create a strategy that is tailored to your unique business
                 goals. From SEO to social media marketing, our solutions will help you reach your target audience and
                 drive conversions. Don't let your competition get ahead. Let us help you succeed today.</p>
