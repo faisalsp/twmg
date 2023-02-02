1b78af48dd983bb8cbe7049a7d2d220642c5f677
faisal <faisal@twmg.com.au>
2023-02-02 16:26:35 +0700

    add slick only on blog page

--- wp-content/themes/twmg/functions.php
+++ wp-content/themes/twmg/functions.php
@@ -138,21 +138,23 @@ function genesis_sample_enqueue_scripts_styles()
 		'all'
 	);
 
-	wp_enqueue_style(
-		genesis_get_theme_handle() . '-slick',
-		'//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css',
-		[],
-		genesis_get_theme_version(),
-		'all'
-	);
+	if (is_page('blog')) {
+		wp_enqueue_style(
+			genesis_get_theme_handle() . '-slick',
+			'//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css',
+			[],
+			genesis_get_theme_version(),
+			'all'
+		);
 
-	wp_enqueue_script(
-		genesis_get_theme_handle() . '-slick',
-		'//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js',
-		[],
-		genesis_get_theme_version(),
-		true
-	);
+		wp_enqueue_script(
+			genesis_get_theme_handle() . '-slick',
+			'//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js',
+			[],
+			genesis_get_theme_version(),
+			true
+		);
+	}
 
 	if (genesis_is_amp()) {
 		wp_enqueue_style(
